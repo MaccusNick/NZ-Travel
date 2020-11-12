5d89bb8773e37c116ddd9b47ebe143ef0e04c04c
MaccusNick <maccus0329@gmail.com>
2020-11-13 00:20:12 +1300

    Header bar

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+    <div class="header-left">Back</div>
+    <div class="header-input">City/Tourist spots/Events</div>
+    <div class="header-right">City</div>
+   </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+.header
+  display flex
+  line-height: .86rem
+  background-color: #00bcd4
+  color: #fff
+.header-left
+  width: .64rem
+  float: left
+.header-input
+ flex:1
+ height: .64rem
+ line-height: .64rem
+ margin-top: .12rem
+ margin-left : .2rem
+ background: #fff
+ border-radius: .1rem
+ color: #ccc
+.header-right
+  width: 1.24rem
+  float: right
+  text-align: center
+</style>
