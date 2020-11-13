42c6fc5f2ce231b788c4abd9c73f3565a2755b45
MaccusNick <maccus0329@gmail.com>
2020-11-13 23:47:24 +1300

    Carousel

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
