fd318aa46f961d0019ea5a85b0d6ebc88441d3c3
MaccusNick <maccus0329@gmail.com>
2020-11-13 18:24:56 +1300

    Header Bar Update

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,19 @@
 <template>
     <div class="header">
-    <div class="header-left">Back</div>
-    <div class="header-input">City/Tourist spots/Events</div>
-    <div class="header-right">City</div>
+    <div class="header-left">
+       <div class="iconfont back-icon">&#xe609;</div>
+    </div>
+
+    <div class="header-input">
+      <span class="iconfont">&#xe6a4;</span>
+      City/Tourist spots/Events
+      </div>
+
+    <div class="header-right">
+      City
+      <span class="iconfont arrow-icon">&#xe877;</span>
+    </div>
+
    </div>
 </template>
 
@@ -13,20 +24,25 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+@import '~styles/varibles.styl';
 .header
   display flex
   line-height: .86rem
-  background-color: #00bcd4
+  background-color: $bgColor
   color: #fff
 .header-left
   width: .64rem
   float: left
+  .back-icon
+    text-align: center
+    font-size: .4rem
 .header-input
  flex:1
  height: .64rem
  line-height: .64rem
  margin-top: .12rem
- margin-left : .2rem
+ margin-left: .2rem
+ padding-left: .2rem
  background: #fff
  border-radius: .1rem
  color: #ccc
@@ -34,4 +50,7 @@ export default {
   width: 1.24rem
   float: right
   text-align: center
+  .arrow-icon
+    margin-left: -.08rem
+    font-size: .35rem
 </style>
